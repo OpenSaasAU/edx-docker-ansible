ad4f5f070e42f63f459a427d851ce446cfc68820
Joshua Calder <josh@opensaas.com.au>
2017-06-10 19:01:07 +1000

    Initial 0.9 push

--- ansible-build.sh
+++ ansible-build.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-ansible-container --var-file container_vars.yml build  -- -t "install:base,install:system-requirements,install:configuration,install:app-requirements,install:code,manage:app-users,migrate,gather_static_assets,assets:gather" -e@../edxapp_vars.yml 
+ansible-container --debug --var-file container_vars.yml build  --use-local-python -- -t "install:base,install:system-requirements,install:configuration,install:app-requirements,install:code,manage:app-users,assets:gather" -e@edxapp_vars.yml 
