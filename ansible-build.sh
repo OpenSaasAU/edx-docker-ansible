a39859f5d4d83f433d493a44a3e022aa5934f9d5
Joshua Calder <josh@opensaas.com.au>
2017-07-17 10:52:13 +1000

    Fix a few errors

--- ansible-build.sh
+++ ansible-build.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-ansible-container --debug --var-file container_vars.yml build  --use-local-python -- -t "install:base,install:system-requirements,install:configuration,install:app-requirements,install:code,manage:app-users,assets:gather" -e@edxapp_vars.yml 
+ansible-container --var-file container_vars.yml build  --use-local-python -- -t "install:base,install:system-requirements,install:configuration,install:app-requirements,install:code" -e@edxapp_vars.yml 
