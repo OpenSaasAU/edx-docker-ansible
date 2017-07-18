f7bba362d9603bccdd21978037be377e2bfb0cf9
Joshua Calder <josh@opensaas.com.au>
2017-07-18 20:39:47 +1000

    Added --debug flag required for ansible-container bug

--- ansible-build.sh
+++ ansible-build.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-ansible-container --var-file container_vars.yml build  --use-local-python -- -t "install:base,install:system-requirements,install:configuration,install:app-requirements,install:code" -e@edxapp_vars.yml 
+ansible-container --debug --var-file container_vars.yml build  --use-local-python -- -t "install:base,install:system-requirements,install:configuration,install:app-requirements,install:code" -e@edxapp_vars.yml 
